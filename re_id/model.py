54409d061e4f8a89033ed71cc2eceb3741df4818
guk98 <guk987@naver.com>
2023-07-12 01:50:11 +0000

    feat: re-identification에 필요한 모델 구축

---
+++ re_id/model.py
@@ -0,0 +1,169 @@
+import torch
+import timm
+import open_clip
+import numpy as np
+import torch.nn as nn
+              
+
+class MobileNetV3(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.mobilenetv3 = timm.create_model('mobilenetv3_large_100.miil_in21k_ft_in1k')
+    
+    def forward(self, x):
+        embedding_feature = self.mobilenetv3(x)
+        return embedding_feature
+
+class ConvNextV2_A(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.convnextv2_t = timm.create_model('convnextv2_atto.fcmae_ft_in1k')
+     
+    def forward(self, x):
+        embedding_feature = self.convnextv2_t(x)
+        return embedding_feature
+    
+class ConvNextV2_F(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.convnextv2_f = timm.create_model('convnextv2_femto.fcmae_ft_in1k')
+     
+    def forward(self, x):
+        embedding_feature = self.convnextv2_f(x)
+        return embedding_feature
+    
+class ConvNextV2_P(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.convnextv2_p = timm.create_model('convnextv2_pico.fcmae_ft_in1k')
+     
+    def forward(self, x):
+        embedding_feature = self.convnextv2_p(x)
+        return embedding_feature
+    
+class ConvNextV2_N(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.convnextv2_n = timm.create_model('convnextv2_nano.fcmae_ft_in22k_in1k')
+     
+    def forward(self, x):
+        embedding_feature = self.convnextv2_n(x)
+        return embedding_feature
+
+class ConvNextV2_T(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.convnextv2_t = timm.create_model('convnextv2_tiny.fcmae_ft_in22k_in1k')
+     
+    def forward(self, x):
+        embedding_feature = self.convnextv2_t(x)
+        return embedding_feature
+
+class ConvNextV2_B(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.convnextv2_b = timm.create_model('convnextv2_base.fcmae_ft_in22k_in1k')
+     
+    def forward(self, x):
+        embedding_feature = self.convnextv2_b(x)
+        return embedding_feature
+    
+class ConvNextV2_L(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.convnextv2_l = timm.create_model('convnextv2_large.fcmae_ft_in22k_in1k')
+     
+    def forward(self, x):
+        embedding_feature = self.convnextv2_l(x)
+        return embedding_feature
+    
+class ConvNextV2_H(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.convnextv2_h = timm.create_model('convnextv2_huge.fcmae_ft_in1k')
+     
+    def forward(self, x):
+        embedding_feature = self.convnextv2_h(x)
+        return embedding_feature
+    
+
+'''
+Code reference : https://github.com/KonradHabel/clip_reid
+'''
+
+class TimmModel(nn.Module):
+
+    def __init__(self, 
+                 model_name='convnextv2_huge.fcmae_ft_in1k',
+                 pretrained=True):
+        super(TimmModel, self).__init__()
+        
+        self.model = timm.create_model(model_name, pretrained=pretrained, num_classes=0) 
+        self.model.logit_scale = torch.nn.Parameter(torch.ones([]) * np.log(1 / 0.07))
+            
+    def forward(self, img1, img2=None):
+        
+        if img2 is not None:
+            images = torch.cat([img1, img2], dim=0)
+            image_features = self.model(images)
+            
+            image_features1 = image_features[:len(img1), :]
+            image_features2 = image_features[len(img1):, :]
+            
+            return image_features1, image_features2
+            
+        else:
+            image_features = self.model(img1)
+            
+        return image_features
+    
+class OpenClipModel(nn.Module):
+
+    def __init__(self,
+                 model_name='ViT-L-14',
+                 pretrained=True,
+                 remove_proj=True):
+        super(OpenClipModel, self).__init__()
+        
+        self.model_name = model_name
+        
+        self.model = open_clip.create_model(model_name,
+                                            pretrained=pretrained)  
+         
+        # delete text parts of clip model
+        del(self.model.transformer)
+        del(self.model.token_embedding)
+        del(self.model.ln_final)
+        del(self.model.positional_embedding)
+        del(self.model.text_projection)
+        
+        if remove_proj and "ViT" in model_name:
+            width, output_dim = self.model.visual.proj.shape
+            print("Remove Projection Layer - old output size: {} - new output size: {}".format(output_dim, width))
+            self.model.visual.proj = None
+
+    def set_grad_checkpoint(self, enable=True): 
+        if "ViT" in self.model_name:
+            self.model.visual.set_grad_checkpointing(enable)
+            print("Use Gradient Checkpointing for {}".format(self.model_name))
+        else:
+            print("Gradient Checkpointing not available for {}".format(self.model_name))
+        
+    def get_image_size(self):
+            return self.model.visual.image_size
+    
+    def forward(self, img1, img2=None):
+        
+        if img2 is not None:
+            images = torch.cat([img1, img2], dim=0)
+            image_features = self.model.encode_image(images)
+            
+            image_features1 = image_features[:len(img1), :]
+            image_features2 = image_features[len(img1):, :]
+            
+            return image_features1, image_features2
+            
+        else:
+            image_features = self.model.encode_image(img1)
+            
+        return image_features
