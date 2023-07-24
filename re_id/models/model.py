560f27b4ad5583e70d847f2ff3fcb9b84bfb89ff
임재규_T5174 <jaekyu.1998.bliz@gmail.com>
2023-07-24 17:14:58 +0900

    refactor: change import module path

--- re_id/models/model.py
+++ re_id/models/model.py
@@ -1,11 +1,17 @@
+import os
+import sys
 import torch
 import timm
 import open_clip
 import numpy as np
 import torch.nn as nn
 import torchvision.models as models
-from ..module.cbam import SAM, CAM, CBAM
+
+from module.cbam import SAM, CAM, CBAM
 from torchvision.models.squeezenet import SqueezeNet1_1_Weights
+
+SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(os.path.dirname(SCRIPT_DIR))
 
 class SqueezeNet(nn.Module):
     def __init__(self):
